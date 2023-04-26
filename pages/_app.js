cf58838f01875a10f4e8c496d87295024f3dee3f
leuzaoo <leuzaoocontato@gmail.com>
2023-04-26 14:12:47 -0300

    Included Roboto font

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,22 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 
+import { Roboto } from "next/font/google";
+const roboto = Roboto({
+  weight: ["100", "400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
+
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <main className={roboto.className}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </main>
   );
 }
