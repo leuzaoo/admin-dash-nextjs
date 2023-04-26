702cfcf4bacc81c6cdfbedec5c1f88732510afb2
leuzaoo <leuzaoocontato@gmail.com>
2023-04-26 15:45:47 -0300

    Created new page

---
+++ components/Layout.js
@@ -0,0 +1,29 @@
+import Navbar from "@/components/Navbar";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function Layout({ children }) {
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            onClick={() => signIn("google")}
+            className="bg-white p-2 px-4 rounded-lg"
+          >
+            Login with Google
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="bg-amber-500 min-h-screen flex">
+      <Navbar />
+      <div className="bg-white flex-grow my-2 mr-2 rounded-lg p-4">
+        {children}
+      </div>
+    </div>
+  );
+}
