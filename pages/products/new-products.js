347bbf0aa7341130600cbb7d1062a31dfb588880
leuzaoo <leuzaoocontato@gmail.com>
2023-04-26 23:02:29 -0300

    Created new page file

---
+++ pages/products/new-products.js
@@ -0,0 +1,12 @@
+import Layout from "@/src/components/Layout";
+import Link from "next/link";
+
+import { PlusSmallIcon } from "@heroicons/react/24/outline";
+
+export default function Products({ icon: Icon }) {
+  return (
+    <Layout>
+      new products page
+    </Layout>
+  );
+}
