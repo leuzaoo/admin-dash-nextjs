69c97f60577e54d69c17ab67c896c4c73e7d498d
leuzaoo <leuzaoocontato@gmail.com>
2023-04-26 22:07:21 -0300

    Updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import MenuItem from "./MenuItem";
 
@@ -13,6 +14,10 @@ import {
 export default function Navbar() {
   const inactiveLink = "p-1";
   const activeLink = `${inactiveLink} bg-yellow-700 rounded-l-lg`;
+  const router = useRouter();
+
+  const { pathname } = router;
+
   return (
     <aside>
       <div className="pt-4 pl-4">
@@ -21,16 +26,32 @@ export default function Navbar() {
         </div>
 
         <div className="flex flex-col gap-8">
-          <Link href={"/"} className={activeLink}>
+          <Link
+            href={"/"}
+            className={pathname === "/" ? activeLink : inactiveLink}
+          >
             <MenuItem text="Dashboard" icon={HomeIcon} />
           </Link>
-          <Link href={"/produtos"} className={inactiveLink}>
+          <Link
+            href={"/products"}
+            className={
+              pathname.includes("/products") ? activeLink : inactiveLink
+            }
+          >
             <MenuItem text="Produtos" icon={ListBulletIcon} />
           </Link>
-          <Link href={"/pedidos"} className={inactiveLink}>
+          <Link
+            href={"/orders"}
+            className={pathname.includes("/orders") ? activeLink : inactiveLink}
+          >
             <MenuItem text="Pedidos" icon={PaperClipIcon} />
           </Link>
-          <Link href={"/configuracoes"} className={inactiveLink}>
+          <Link
+            href={"/settings"}
+            className={
+              pathname.includes("/settings") ? activeLink : inactiveLink
+            }
+          >
             <MenuItem text="Configurações" icon={Cog6ToothIcon} />
           </Link>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
 import Layout from "@/components/Layout";
+import Image from "next/image";
+
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  return <Layout>test</Layout>;
+  const { data: session } = useSession();
+  return (
+    <Layout>
+      <div className="text-black flex justify-between">
+        <h2>
+          Olá, <b>{session?.user?.name}.</b>
+        </h2>
+
+        <div>
+          <Image
+            src={session?.user?.image}
+            alt="User image."
+            className="w-16 h-16"
+            width={600}
+            height={800}
+          />
+        </div>
+      </div>
+    </Layout>
+  );
 }
