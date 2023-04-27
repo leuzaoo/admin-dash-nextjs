56610ea481179d16519266d74d8e22119d2423c7
leuzaoo <leuzaoocontato@gmail.com>
2023-04-27 12:51:17 -0300

    Updated

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['lh3.googleusercontent.com']
-  }
 };
 
 module.exports = nextConfig;
