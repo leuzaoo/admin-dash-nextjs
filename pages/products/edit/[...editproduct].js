79c8c8fa84056ebab2567aec0689a967019a6010
leuzaoo <leuzaoocontato@gmail.com>
2023-04-29 18:41:11 -0300

    Create edit product page

---
+++ pages/products/edit/[...editproduct].js
@@ -0,0 +1,9 @@
+import Layout from "@/src/components/Layout";
+
+export default function EditProductPage() {
+    return (
+        <Layout>
+            edit product here
+        </Layout>
+    )
+}
