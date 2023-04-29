87889456347d89787d8dbe5c813b69f996ece4f0
leuzaoo <leuzaoocontato@gmail.com>
2023-04-29 17:37:57 -0300

    Fix error

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,4 @@
-import { Schema, model } from "mongoose";
+import { Schema, model, models } from "mongoose";
 
 const ProductSchema = new Schema({
   title: { type: String, required: true },
@@ -6,4 +6,4 @@ const ProductSchema = new Schema({
   price: { type: Number, required: true },
 });
 
-export const Product = model("Product", ProductSchema);
+export const Product = models.Product || model("Product", ProductSchema);
