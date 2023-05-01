a95c8f917e9afc255eb544a7b5bd96882c021014
leuzaoo <leuzaoocontato@gmail.com>
2023-05-01 15:18:06 -0300

    Updated

--- pages/api/products.js
+++ pages/api/products.js
@@ -6,7 +6,11 @@ export default async function handle(req, res) {
   await mongooseConnect();
 
   if (method === "GET") {
-    res.json(await Product.find());
+    if (req.query?.id) {
+      res.json(await Product.findOne({ _id: req.query.id }));
+    } else {
+      res.json(await Product.find());
+    }
   }
 
   if (method === "POST") {
--- pages/products/new.js
+++ pages/products/new.js
@@ -1,57 +1,5 @@
-import Layout from "@/src/components/Layout";
-import axios from "axios";
-import { useRouter } from "next/router";
-import { useState } from "react";
+import ProductForm from "@/src/components/ProductForm";
 
 export default function NewProduct() {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [price, setPrice] = useState("");
-  const [goToProducts, setGoToProducts] = useState(false);
-  const router = useRouter();
-
-  async function createProduct(ev) {
-    ev.preventDefault();
-    const data = { title, description, price };
-    await axios.post("/api/products", data);
-    setGoToProducts(true);
-  }
-  if (goToProducts) {
-    router.push("/products");
-  }
-  return (
-    <Layout>
-      <form onSubmit={createProduct}>
-        <div className="border-[1px] p-4 rounded flex flex-col gap-2">
-          <label className="text-sm font-bold">Informações do Produto*</label>
-          <input
-            type="text"
-            placeholder="Nome"
-            value={title}
-            onChange={(ev) => setTitle(ev.target.value)}
-          />
-          <textarea
-            placeholder="Descrição"
-            value={description}
-            onChange={(ev) => setDescription(ev.target.value)}
-          />
-
-          <label className="text-sm font-bold">Preço*</label>
-          <input
-            type="number"
-            placeholder="1.999,90"
-            value={price}
-            onChange={(ev) => setPrice(ev.target.value)}
-          />
-
-          <button
-            type="submit"
-            className="bg-black text-white rounded-lg center w-32 px-4 py-2 transition-all duration-300 hover:bg-blue-500 hover:transition-all hover:duration-300"
-          >
-            Adicionar
-          </button>
-        </div>
-      </form>
-    </Layout>
-  );
+  return <ProductForm />;
 }
