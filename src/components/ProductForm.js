d7ab9758660d45d30996f6fe61955636fe89771c
leuzaoo <leuzaoocontato@gmail.com>
2023-05-01 17:09:35 -0300

    Fix error

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+
 export default function ProductForm() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
