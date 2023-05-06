daf0c61864e209ccc9542d59b25e89407db218fd
leuzaoo <leuzaoocontato@gmail.com>
2023-05-06 20:44:06 -0300

    Updated edit page

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import axios from "axios";
 
 export default function ProductForm({
+  _id,
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
@@ -13,10 +14,16 @@ export default function ProductForm({
   const [goToProducts, setGoToProducts] = useState(false);
   const router = useRouter();
 
-  async function createProduct(ev) {
+  async function saveProduct(ev) {
     ev.preventDefault();
     const data = { title, description, price };
-    await axios.post("/api/products", data);
+    if (_id) {
+      //update
+      await axios.put("/api/products", {...data, _id});
+    } else {
+      //create
+      await axios.post("/api/products", data);
+    }
     setGoToProducts(true);
   }
   if (goToProducts) {
@@ -24,7 +31,7 @@ export default function ProductForm({
   }
 
   return (
-    <form onSubmit={createProduct}>
+    <form onSubmit={saveProduct}>
       <div className="border-[1px] p-4 rounded flex flex-col gap-2">
         <label className="text-sm font-bold">Informações do Produto*</label>
         <input
@@ -51,7 +58,7 @@ export default function ProductForm({
           type="submit"
           className="bg-black text-white rounded-lg center w-32 px-4 py-2 transition-all duration-300 hover:bg-blue-500 hover:transition-all hover:duration-300"
         >
-          Adicionar
+          Salvar
         </button>
       </div>
     </form>
