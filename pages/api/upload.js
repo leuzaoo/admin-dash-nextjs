a821815a17da9f868d77300f351fe34f1efbbff7
leuzaoo <leuzaoocontato@gmail.com>
2023-05-07 22:52:43 -0300

    Updated code to accept images

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,5 +1,7 @@
 import multiparty from "multiparty";
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import fs from "fs";
+import mime from "mime-types";
 
 const bucketName = "aitrend-admin-panel";
 
@@ -19,16 +21,24 @@ export default async function handle(req, res) {
       secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
     },
   });
+  const links = [];
   for (const file of files.file) {
     const ext = file.originalFilename.split(".").pop();
+    const newFilename = Date.now() + "." + ext;
+    console.log({ ext, file });
     await client.send(
       new PutObjectCommand({
         Bucket: bucketName,
-        Key: "",
+        Key: newFilename,
+        Body: fs.readFileSync(file.path),
+        ACL: "public-read",
+        ContentType: mime.lookup(file.path),
       })
     );
+    const link = `https://${bucketName}.s3.amazonaws.com/${newFilename}`;
+    links.push(link);
   }
-  return res.json("ok");
+  return res.json({ links });
 }
 
 export const config = {
