13831baeca7ba89392e04c590bdd805419073fe8
leuzaoo <leuzaoocontato@gmail.com>
2023-05-11 17:21:04 -0300

    Updated

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -38,7 +38,7 @@ export default function Products() {
           </thead>
           <tbody>
             {products.map((product) => (
-              <tr>
+              <tr key={product._id}>
                 <td>{product.title}</td>
                 <td>
                   <Link
