fbdd6f59f827f6a136957a13dd6fedc69abd7a6d
leuzaoo <leuzaoocontato@gmail.com>
2023-05-13 11:27:31 -0300

    Create new SaveButton component

---
+++ src/components/SaveButton.js
@@ -0,0 +1,9 @@
+export default function SaveButton({ text }) {
+  return (
+    <div>
+      <button className="py-2 px-4 bg-black text-white rounded-lg">
+        {text}
+      </button>
+    </div>
+  );
+}
