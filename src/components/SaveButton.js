ed4a8d4abdb319e8ae537380033e3408a78f4edb
leuzaoo <leuzaoocontato@gmail.com>
2023-05-13 12:12:41 -0300

    Updated button component

--- src/components/SaveButton.js
+++ src/components/SaveButton.js
@@ -1,7 +1,10 @@
-export default function SaveButton({ text }) {
+export default function SaveButton({ text, type }) {
   return (
     <div>
-      <button className="py-2 px-4 bg-black text-white rounded-lg">
+      <button
+        type={type}
+        className="py-2 px-4 bg-black text-white rounded-lg duration-300 hover:bg-blue-600 hover:duration-300"
+      >
         {text}
       </button>
     </div>
