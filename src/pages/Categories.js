eddf095b824d275cab11f733be67285fe1a271e3
leuzaoo <leuzaoocontato@gmail.com>
2023-05-15 20:01:25 -0300

    Updated global css from filter categories

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -8,15 +8,19 @@ import { useEffect, useState } from "react";
 export default function Categories() {
   const [name, setName] = useState("");
   const [categories, setCategories] = useState([]);
-  useEffect(() => {}, [
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+  function fetchCategories() {
     axios.get("api/categories").then((result) => {
       setCategories(result.data);
-    }),
-  ]);
+    });
+  }
   async function saveCategory(ev) {
     ev.preventDefault();
     await axios.post("/api/categories", { name });
     setName("");
+    fetchCategories();
   }
   return (
     <Layout>
@@ -30,6 +34,14 @@ export default function Categories() {
           onChange={(ev) => setName(ev.target.value)}
           value={name}
         />
+        <select>
+          <option value="">Seleciona uma categoria</option>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              // eslint-disable-next-line react/jsx-key
+              <option value={category._id}>{category.name}</option>
+            ))}
+        </select>
         <SaveButton type="submit" text="Salvar" />
       </form>
       <table className="basic mt-4">
