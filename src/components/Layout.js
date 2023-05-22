c6e3cd830d5ebe6ef53bcb5b446dbd11176e17b6
leuzaoo <leuzaoocontato@gmail.com>
2023-05-22 19:20:07 -0300

    Updated background color

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -18,7 +18,7 @@ export default function Layout({ children }) {
   }
 
   return (
-    <div className="bg-slate-300 min-h-screen flex">
+    <div className="bg-gray-200 min-h-screen flex">
       <Navbar />
       <div className="bg-white flex-grow my-2 mr-2 rounded-lg p-4">
         {children}
