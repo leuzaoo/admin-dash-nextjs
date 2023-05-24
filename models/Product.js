155dae8e4461f361434c4e4d3cf050f8f5b26856
leuzaoo <leuzaoocontato@gmail.com>
2023-05-24 19:12:36 -0300

    Add properties to product schema

--- models/Product.js
+++ models/Product.js
@@ -5,7 +5,8 @@ const ProductSchema = new Schema({
   description: String,
   price: { type: Number, required: true },
   images: [{ type: String }],
-  category: {type:mongoose.Types.ObjectId, ref:'Category'},
+  category: { type: mongoose.Types.ObjectId, ref: "Category" },
+  properties: { type: Object },
 });
 
 export const Product = models.Product || model("Product", ProductSchema);
