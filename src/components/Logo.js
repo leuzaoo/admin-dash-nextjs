f84814564ea8f817c4f04489484004ad15940faa
leuzaoo <leuzaoocontato@gmail.com>
2023-05-31 20:01:28 -0300

    New logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,9 @@
+export default function Logo() {
+  return (
+    <div className="top-0 pt-4 min-h-screen bg-white my-2 mx-2 rounded-md">
+      <div className="pr-32 flex pt-4 pl-4 pb-4">
+        <img src="/aitrend-logo.png" className="w-24" />
+      </div>
+    </div>
+  );
+}
