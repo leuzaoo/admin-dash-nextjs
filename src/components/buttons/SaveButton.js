ea9989420b607142799a9058f9208ba34c52dc31
leuzaoo <leuzaoocontato@gmail.com>
2023-06-04 17:06:42 -0300

    Updated

--- src/components/buttons/SaveButton.js
+++ src/components/buttons/SaveButton.js
@@ -3,7 +3,7 @@ export default function SaveButton({ text, type }) {
     <div>
       <button
         type={type}
-        className="bg-blue-600 text-white rounded-lg center w-[94.8px] h-[40px] transition-all  hover:bg-black hover:transition-all hover:duration-500 duration-500"
+        className="bg-blue-600 text-white rounded-lg center px-4 py-2 transition-all  hover:bg-black hover:transition-all hover:duration-500 duration-500"
       >
         {text}
       </button>
