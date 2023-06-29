ad1930a255f12c806553638b48bb080ca7b7b0e5
leuzaoo <leuzaoocontato@gmail.com>
2023-06-29 09:16:24 -0300

    Updated orders page

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,9 +1,47 @@
 import Layout from "@/src/components/Layout";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Orders() {
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    axios.get("/api/orders").then((response) => {
+      setOrders(response.data);
+    });
+  });
   return (
     <Layout>
-      orders page
+      <table className="basic">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Destinatário</th>
+            <th>Produto</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.length > 0 &&
+            orders.map((order) => (
+              <tr>
+                <td>{order._id}</td>
+                <td>
+                  {order.name} <br />
+                  {order.email} <br />
+                  {order.phoneNumber} <br />
+                  {order.streetAdress}
+                </td>
+                <td>
+                  {order.line_items.map((l) => (
+                    <>
+                      {l.price_data?.product_data?.name} x {l.quantity}
+                      <br />
+                    </>
+                  ))}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </Layout>
   );
 }
