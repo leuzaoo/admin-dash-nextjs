44e9e09df000bd090e443081f94ef7e5d54db566
leuzaoo <leuzaoocontato@gmail.com>
2023-07-03 19:39:07 -0300

    Updated delete function

--- src/components/buttons/SaveButton.js
+++ src/components/buttons/SaveButton.js
@@ -1,7 +1,8 @@
-export default function SaveButton({ text, type }) {
+export default function SaveButton({ text, type, onClick }) {
   return (
     <div>
       <button
+        onClick={onClick}
         type={type}
         className="bg-blue-600 text-white rounded-lg center px-4 py-2 transition-all  hover:bg-black hover:transition-all hover:duration-500 duration-500"
       >
